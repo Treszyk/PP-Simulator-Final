e77321dede4ac44325d6c24004448d0b24eb4de2
Patryk <79578683+Treszyk@users.noreply.github.com>
2024-10-26 12:24:02 +0200

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,11 +1,27 @@
-﻿using System.Reflection.Emit;
-using System.Xml.Linq;
-
-namespace Simulator;
+﻿namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "Unknown";
+    public required string Description {
+        get { return _description; }
+        init
+        {
+            //Console.WriteLine(value.Length);
+            _description = value.Trim();
+            if (_description.Length > 15)
+            {
+                _description = _description.Remove(15);
+                _description = _description.Trim();
+            }
+            if (_description.Length < 3)
+                _description = _description.PadRight(3, '#');
+            if (char.IsLower(_description[0]))
+            {
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+            }
+        }
+    }
     public uint Size { get; set; } = 3;
     public string Info => $"{Description} [{Size}]";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,8 +2,29 @@
 
 public class Creature
 {
-    string Name { get; set; }
-    int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
+    public string Name { 
+        get {  return _name; }
+        init
+        {
+            //Console.WriteLine(value.Length);
+            _name = value.Trim();
+            if (_name.Length > 25)
+                _name = _name.Remove(25).Trim();     
+            if (_name.Length < 3)
+                _name = _name.PadRight(3, '#');
+            if (char.IsLower(_name[0]))
+                _name = char.ToUpper(_name[0]) + _name.Substring(1);
+        }
+    }
+    public int Level {
+        get { return _level; }
+        init
+        {
+            _level = Math.Clamp(value, 1, 10);
+        } 
+    }
     public string Info => $"{Name} [{Level}]";
     public Creature()
     {
@@ -15,5 +36,5 @@ public class Creature
         Level = level;
     }
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
-    
+    public void Upgrade() => _level = _level < 10 ? _level+1 : _level;
 }
