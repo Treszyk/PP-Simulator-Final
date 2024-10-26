7f0a9d7a2b27474046b581ee3a519c2e8781ef56
Patryk <79578683+Treszyk@users.noreply.github.com>
2024-10-26 11:28:15 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,19 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    string Name { get; set; }
+    int Level { get; set; }
+    public string Info => $"{Name} [{Level}]";
+    public Creature()
+    {
+        
+    }
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    
+}
